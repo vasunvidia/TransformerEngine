2b7652d00e3c3a9a1f85c2ae6418d040c7f6e1b9
Jaemin Choi <jaeminc@nvidia.com>
2024-01-29 18:04:43 -0800

    Revert "Avoid using torch.compile for roll and fill_ (#609)"
    This reverts commit 29413187eb6a84a8032032e7f033371f6f83e47c.

--- transformer_engine/pytorch/fp8.py
+++ transformer_engine/pytorch/fp8.py
@@ -583,7 +583,7 @@ def _update_amax_history(amax_history: torch.Tensor) -> torch.Tensor:
     return amax_history
 
 
-@torch.jit.script
+@jit_fuser
 def _default_get_amax(
     amax_history: torch.Tensor,
     amax_compute_algo: str,
@@ -625,7 +625,7 @@ def _compute_scaling_factor_inverse(
     return torch.where(non_weight_mask, 1.0 / scale, scale_inv)
 
 
-@torch.jit.script
+@jit_fuser
 def _fused_amax_and_scale_update(
     amax_history: torch.Tensor,
     scale: torch.Tensor,
