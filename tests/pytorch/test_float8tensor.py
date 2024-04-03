1f02176676629b094f409c7d11378f2d6fc9fb86
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2024-04-02 19:01:32 -0700

    Fix tests
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- tests/pytorch/test_float8tensor.py
+++ tests/pytorch/test_float8tensor.py
@@ -280,65 +280,33 @@ class TestFloat8Tensor:
         # Perform transpose
         x_fp8_t = x_fp8.transpose_2d()
         x_t = x.transpose(0, 1)
-        if x_fp8_t.dtype == torch.uint8:
-            x_fp8_t = Float8Tensor.make_like(x_fp8, data=x_fp8_t)
+        x_fp8_t = Float8Tensor.make_like(x_fp8, data=x_fp8_t)
 
         # Check results
         tols = dict(rtol=0, atol=0)
         torch.testing.assert_close(x_fp8_t, x_t, **tols)
 
         # Make sure we are not trivially passing the test
-        if transpose_dims[0] != transpose_dims[1]:
-            with pytest.raises(AssertionError):
-                torch.testing.assert_close(
-                    y_fp8,
-                    x_ref,
-                    **tols,
-                )
+        with pytest.raises(AssertionError):
+            torch.testing.assert_close(x_fp8_t, x, **tols)
 
-        # Check transpose caching
-        if x_fp8.dim() == 2 and transpose_dims[0] != transpose_dims[1]:
+        # Caching test.
+        assert x_fp8._transpose_invalid, "Transpose cache must be invalid when not caching."
+        x_fp8 += 0.5
+        x = x_fp8.from_float8()
+        x_fp8_t = Float8Tensor.make_like(x_fp8, data=x_fp8.transpose_2d(cache=True))
+        x_t = x.transpose(0, 1)
+        torch.testing.assert_close(x_fp8_t, x_t, **tols)
+        assert not x_fp8._transpose_invalid, "Transpose cache reset incorrectly."
 
-            # Check that cached transpose is returned when expected
-            # Note: Sneakily destroy data so that recalculating
-            # transpose would give wrong answer.
-            x_fp8 += 0.5
-            x_ref = x_fp8.from_float8()
-            torch.testing.assert_close(
-                x_fp8.transpose(*transpose_dims, update_cache="lazy"),
-                x_ref.transpose(*transpose_dims),
-                **tols,
-            )
-            x_fp8_data = x_fp8._data.clone()
-            x_fp8._data.zero_()
-            torch.testing.assert_close(
-                x_fp8.transpose(*transpose_dims),
-                x_ref.transpose(*transpose_dims),
-                **tols,
-            )
-            torch.testing.assert_close(
-                x_fp8.transpose(*transpose_dims, update_cache="lazy"),
-                x_ref.transpose(*transpose_dims),
-                **tols,
-            )
-            torch.testing.assert_close(
-                x_fp8.transpose(*transpose_dims, update_cache="force"),
-                torch.zeros_like(x_ref.transpose(*transpose_dims)),
-                rtol=0,
-                atol=0,
-            )
-            x_fp8._data.copy_(x_fp8_data)
-            x_fp8._reset_caches()
-
-            # Make sure cache is reset after in-place operation
-            x_fp8.transpose(*transpose_dims, update_cache="force")
-            x_fp8 += 0.5
-            x_ref = x_fp8.from_float8()
-            torch.testing.assert_close(
-                x_fp8.transpose(*transpose_dims),
-                x_ref.transpose(*transpose_dims),
-                **tols,
-            )
+        # Inplace update test.
+        x_fp8 += 0.5
+        assert x_fp8._transpose_invalid, "Transpose cache not invalidated properly."
+        x = x_fp8.from_float8()
+        x_fp8_t = Float8Tensor.make_like(x_fp8, data=x_fp8.transpose_2d(cache=True))
+        x_t = x.transpose(0, 1)
+        torch.testing.assert_close(x_fp8_t, x_t, **tols)
+        assert not x_fp8._transpose_invalid, "Transpose cache reset incorrectly."
 
     def test_serialization(
         self,
