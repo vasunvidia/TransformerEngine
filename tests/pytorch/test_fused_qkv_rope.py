9ca7430c41f070d2e31784ad4ef71aee706c10bb
Vasudevan Rengasamy <vrengasamy@nvidia.com>
2025-08-24 01:39:05 -0700

    Cleanup test

--- tests/pytorch/test_fused_qkv_rope.py
+++ tests/pytorch/test_fused_qkv_rope.py
@@ -24,25 +24,12 @@ def _non_overlapping_grad(query: torch.Tensor, key: torch.Tensor, value: torch.T
     t3 = torch.ones_like(value)
     return torch.sum(query * t1) + torch.sum(key * t2) + torch.sum(value * t3)
 
-# @pytest.mark.parametrize("start_positions", [True, False])
-# @pytest.mark.parametrize("dtype", [torch.float32, torch.bfloat16, torch.float16])
-# @pytest.mark.parametrize("seq_length", [2048, 4096])
-# @pytest.mark.parametrize("hidden_size", [128, 256])
-# @pytest.mark.parametrize("rotary_percent", [0.5, 1.0])
-# @pytest.mark.parametrize("margin", [0, 10])
-# @pytest.mark.parametrize("transpose", [None, (0, 1), (2, 3)])
-# @pytest.mark.parametrize("tensor_format", ["sbhd", "bshd"])
-# @pytest.mark.parametrize("loss_func", [_overlapping_grad, _non_overlapping_grad])
-# @pytest.mark.parametrize("cp_size", [1, 2])
-# @pytest.mark.parametrize("interleaved", [True, False])
-
 @pytest.mark.parametrize("start_positions", [True, False])
 @pytest.mark.parametrize("dtype", [torch.float32, torch.bfloat16, torch.float16])
 @pytest.mark.parametrize("seq_length", [2, 8, 2048, 4096])
 @pytest.mark.parametrize("hidden_size", [64, 128, 256])
 @pytest.mark.parametrize("rotary_percent", [0.5, 1.0])
 @pytest.mark.parametrize("margin", [0, 10])
-#@pytest.mark.parametrize("transpose", [None, (0, 1), (2, 3)])
 @pytest.mark.parametrize("tensor_format", ["sbhd", "bshd"])
 @pytest.mark.parametrize("loss_func", [_overlapping_grad, _non_overlapping_grad])
 @pytest.mark.parametrize("cp_size", [1, 2])
@@ -53,7 +40,6 @@ def test_fused_qkv_rope(
     hidden_size: int,
     rotary_percent: float,
     margin: int,
-    #transpose: Union[Tuple, None],
     tensor_format: str,
     loss_func: Callable,
     cp_size: int,
@@ -202,125 +188,5 @@ def test_fused_qkv_rope(
         torch.testing.assert_close(key_fused, key_unfused)
         torch.testing.assert_close(value_fused, value_unfused)
 
-        #grad_q_unfused, grad_k_unfused, grad_v_unfused = torch.split(grad_unfused, [hidden_size*4, hidden_size, hidden_size], dim=3)
-        #grad_q_fused, grad_k_fused, grad_v_fused = torch.split(grad_fused, [hidden_size*4, hidden_size, hidden_size], dim=3)
-        #torch.testing.assert_close(grad_q_fused, grad_q_unfused)
-        #torch.testing.assert_close(grad_k_fused, grad_k_unfused)
-        #torch.testing.assert_close(grad_v_fused, grad_v_unfused)
-
-#        print_diff(grad_v_unfused, grad_v_fused, "grad_v", verbose=True, batch_idx_print=batch_idx_print, seq_idx_print=seq_idx_print, h_idx_print=h_idx_print)
-
         if not isinstance(start_positions, torch.Tensor):
             torch.testing.assert_close(grad_fused, grad_unfused)
-
-
-#@pytest.mark.parametrize("margin", [10])
-#@pytest.mark.parametrize("start_positions", [True, False])
-#@pytest.mark.parametrize("dtype", [torch.float32, torch.bfloat16, torch.float16])
-#@pytest.mark.parametrize("hidden_size", [128, 256])
-#@pytest.mark.parametrize("rotary_percent", [0.5, 1.0])
-#@pytest.mark.parametrize("transpose", [None, (1, 2)])
-#@pytest.mark.parametrize("loss_func", [_overlapping_grad, _non_overlapping_grad])
-#@pytest.mark.parametrize("cp_size", [1, 2])
-#@pytest.mark.parametrize("interleaved", [True, False])
-#def test_fused_rope_thd(
-#    dtype: torch.dtype,
-#    hidden_size: int,
-#    rotary_percent: float,
-#    transpose: Union[Tuple, None],
-#    loss_func: Callable,
-#    cp_size: int,
-#    interleaved: bool,
-#    start_positions: bool,
-#    margin: int,
-#) -> None:
-#
-#    if start_positions == True and cp_size > 1:
-#        # `start_positions` is only supported for `cp_size=1` and inference.
-#        pytest.skip("Skipping test with cp_size>1 and start_positions=True")
-#
-#    device = torch.device("cuda:0")
-#    batch_size, head_num = 2, 64
-#    cu_seqlens = [0, 400, 542, 711, 727, 752, 1270, 1426, 1450, 1954, 2044, 2048]
-#
-#    # Get arbitrary offsets to be used with RoPE for all the sequences
-#    start_positions = (
-#        torch.randint(0, margin, (len(cu_seqlens) - 1,), dtype=torch.int32, device=device)
-#        if start_positions
-#        else None
-#    )
-#
-#    if cp_size > 1:
-#        cu_seqlens_padded = [0]
-#        for i in range(1, len(cu_seqlens)):
-#            cu_seqlens_padded.append(
-#                cu_seqlens_padded[i - 1]
-#                + math.ceil((cu_seqlens[i] - cu_seqlens[i - 1]) / (cp_size * 2)) * (cp_size * 2)
-#            )
-#    else:
-#        cu_seqlens_padded = cu_seqlens
-#    cu_seqlens_padded = torch.tensor(
-#        cu_seqlens_padded,
-#        dtype=torch.int32,
-#        device=device,
-#    )
-#    t = torch.rand(
-#        (cu_seqlens_padded[-1] // cp_size, head_num, hidden_size),
-#        dtype=dtype,
-#        device=device,
-#    )
-#    if transpose:
-#        t = t.transpose(*transpose).contiguous().transpose(*transpose)
-#    t.requires_grad = True
-#
-#    rotary_pos_emb = RotaryPositionEmbedding(hidden_size, rotary_percent, interleaved=interleaved)
-#    emb = rotary_pos_emb(cu_seqlens_padded[-1])
-#    assert emb.is_contiguous()
-#
-#    for cp_rank in range(cp_size):
-#        # unfused
-#        # The fused kernel computes in float32 internally, so we force the unfused func to use float32
-#        # for more accurate comparison
-#        output_unfused = apply_rotary_pos_emb(
-#            t.float(),
-#            emb,
-#            start_positions=start_positions,
-#            tensor_format="thd",
-#            interleaved=interleaved,
-#            fused=False,
-#            cu_seqlens=cu_seqlens_padded,
-#            cp_size=cp_size,
-#            cp_rank=cp_rank,
-#        ).to(dtype)
-#        loss_unfused = loss_func(output_unfused)
-#
-#        if not isinstance(start_positions, torch.Tensor):
-#            loss_unfused.backward()
-#            grad_unfused = t.grad.detach().clone()
-#        t.grad = None
-#
-#        # fused
-#        output_fused = apply_rotary_pos_emb(
-#            t,
-#            emb,
-#            start_positions=start_positions,
-#            interleaved=interleaved,
-#            fused=True,
-#            tensor_format="thd",
-#            cu_seqlens=cu_seqlens_padded,
-#            cp_size=cp_size,
-#            cp_rank=cp_rank,
-#        )
-#        loss_fused = loss_func(output_fused)
-#
-#        if not isinstance(start_positions, torch.Tensor):
-#            loss_fused.backward()
-#            grad_fused = t.grad.detach().clone()
-#        t.grad = None
-#
-#        torch.testing.assert_close(output_fused, output_unfused)
-#
-#        if not isinstance(start_positions, torch.Tensor):
-#            torch.testing.assert_close(grad_fused, grad_unfused)
-#
-#        assert output_fused.is_contiguous()
